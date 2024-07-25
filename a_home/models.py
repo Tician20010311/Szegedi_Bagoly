c7d7eca28d76bbca55c89d60a2bd56d4eafaef96
Tokai20010311 <tokaitician.new@gmail.com>
2024-07-25 16:44:40 +0200

    test

--- a_home/models.py
+++ a_home/models.py
@@ -11,7 +11,6 @@ class News(models.Model):
     def __str__(self):
         return f"{self.title}"
 
-
 class Parents(models.Model):
     text = models.CharField(max_length=100, default='')
     link = models.CharField(max_length=255, default='')
